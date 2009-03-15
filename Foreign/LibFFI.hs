6ce20c0a3c0e0b6ef8d2bfa448f57d85c585d57a
rturk <rturk@science.uva.nl>
2009-03-15 01:16:43 +0100

    Add example use to haddock
    Ignore-this: 662242c0567786d66935f78a2e09ab2c
    
    darcs-hash:20090315001643-296f3-860ea9df7ab4ec23124b51460b8a2da70b1276e0.gz

--- Foreign/LibFFI.hs
+++ Foreign/LibFFI.hs
@@ -1,3 +1,19 @@
+{- |
+Allocate 1GB of memory, zero it, and crash:
+
+@
+import System.Posix.DynamicLinker
+import Foreign.Ptr
+import Foreign.LibFFI
+
+main = do
+    malloc <- dlsym Default \"malloc\"
+    memset <- dlsym Default \"memset\"
+    p <- callFFI malloc (retPtr retVoid) [argCSize (2^30)]
+    callFFI memset (retPtr retVoid) [argPtr p, argCInt 0, argCSize (2^30)]
+    callFFI memset (retPtr retVoid) [argPtr nullPtr, argCInt 0, argCSize 1]
+@
+-}
 module Foreign.LibFFI
     (Arg
     ,RetType
